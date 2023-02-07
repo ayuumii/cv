410f8d8c6fded3b9be04444b465c69e86f7aa4dd
ayuumii <98504175+ayuumii@users.noreply.github.com>
2023-02-07 13:26:09 +0800

    2 files modified

--- src/app/skills/skills.component.ts
+++ src/app/skills/skills.component.ts
@@ -6,10 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./skills.component.css']
 })
 export class SkillsComponent implements OnInit {
-  showData($events: any){
-    console.log("Here is my Skills"); if($events){
-      console.log($events.target);
-      console.log($events.target.value);
+  showData($event: any){
+    console.log("Here is my Skills"); if($event){
+      console.log($event.target);
+      console.log($event.target.value);
     }}
   constructor() { }
 
